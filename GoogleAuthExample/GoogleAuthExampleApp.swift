d1455d006c1b9943f6a3975ec760d1d00b02a50a
Christopher Stone <chrisrunner542@gmail.com>
2025-04-29 14:26:17 -0700

    Initial Commit

---
+++ GoogleAuthExample/GoogleAuthExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAuthExampleApp.swift
+//  GoogleAuthExample
+//
+//  Created by MacBook on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct GoogleAuthExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
